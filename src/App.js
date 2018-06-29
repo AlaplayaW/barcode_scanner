33f15f42a78143dcde22949de0fa4965704da4b3
Nicholas Adams <nicholasalexanderadams@gmail.com>
2018-06-29 19:38:09 -0400

    no semicolons;

--- src/App.js
+++ src/App.js
@@ -18,28 +18,71 @@ class App extends Component {
     }
 
     this.onProcessed = this.onProcessed.bind(this)
+    this.onDetected = this.onDetected.bind(this)
     this.startScanning = this.startScanning.bind(this)
     this.stopScanning = this.stopScanning.bind(this)
+    this.captureImage = this.captureImage.bind(this)
   }
 
   quaggaInitCallback(err) {
+
       if (err) {
           console.log(err);
           return
       }
+
       console.log("Initialization finished. Ready to start");
+
       this.onProcessed();
-      Quagga.start();
+      this.onDetected();
+
+      Quagga.start()
+
+      this.setState({
+        canvas: document.querySelector('.input-stream canvas'),
+      })
+
+      this.setState({
+        context: this.state.canvas.getContext('2d'),
+        video: document.querySelector('.input-stream video'),
+      })
   }
 
-  onProcessed() {
-    Quagga.onProcessed(v => {
-      console.log('onProcessed: ', v)
+  displayDuplicateAlert() {
+    window.alert('You have already scanned this barcode.')
+  }
+
+  onDetected() {
+    Quagga.onDetected(v => {
+      this.captureImage()
+      this.stopScanning()
+      this.state.codes.includes(v.codeResult.code)
+        ? this.displayDuplicateAlert()
+        : (() => {
+          this.addDetectedCode(v.codeResult.code)
+        })()
+    })
+  }
+
+  captureImage() {
+    this.state.context.drawImage(this.state.video, 0, 0, this.state.canvas.width, this.state.canvas.height)
+    this.state.canvas.toBlob(blob => {
+      const url = URL.createObjectURL(blob)
+      console.log('url: ', url)
+      this.setState({
+        imageUrls: [...this.state.imageUrls, url]
+      })
+    })
+  }
+
+  addDetectedCode(code) {
+    this.setState({
+      codes: [...this.state.codes, code]
     })
   }
 
   stopScanning() {
-    Quagga.stop();
+    Quagga.stop()
   }
 
   startScanning () {
@@ -79,4 +122,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
