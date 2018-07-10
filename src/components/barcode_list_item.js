b29b91dfe847b7942d19e3156f8a1f5bc98943ce
Nicholas Adams <nicholasalexanderadams@gmail.com>
2018-07-09 22:55:33 -0400

    Reference a stylesheet for this component.

--- src/components/barcode_list_item.js
+++ src/components/barcode_list_item.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import EditableLabel from './editable_label.js'
+import 'components/barcode_list_item/barcode_list_item.css'
+
 
 class BarcodeListItem extends React.Component {
   constructor(props) {
